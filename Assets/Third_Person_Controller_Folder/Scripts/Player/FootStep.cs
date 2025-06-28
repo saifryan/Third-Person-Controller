ddaf040fdb5c388331d0ea85865df6aa10f7c504
saifryan <saif.ryan@gmail.com>
2025-06-28 07:08:28 +0500

    Complete Project
    Complete Project

--- Assets/Third_Person_Controller_Folder/Scripts/Player/FootStep.cs
+++ Assets/Third_Person_Controller_Folder/Scripts/Player/FootStep.cs
@@ -5,18 +5,21 @@ public class FootStep : MonoBehaviour
     // Foot Stop Sound Play
     public void FootStopSoundPlay()
     {
+        if (PopupController.AnyPanelOpen) return;
         SoundManager.Instance.PlaySound(SoundManager.Instance.FootStep);
     }
 
     // Jump Start Sound Play
     public void JumpStartSoundPlay()
     {
+        if (PopupController.AnyPanelOpen) return;
         SoundManager.Instance.PlaySound(SoundManager.Instance.JumpStart);
     }
 
     // Jump End Sound Play
-    public void JumpEndSoundPlay() 
+    public void JumpEndSoundPlay()
     {
+        if (PopupController.AnyPanelOpen) return;
         SoundManager.Instance.PlaySound(SoundManager.Instance.JumpLand);
     }
 }
